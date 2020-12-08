c63e477f4d0ddc36e5a043effe112a1fd9778a6c
Bart Massey <bart.massey@gmail.com>
2020-12-07 17:11:02 -0800

    macroized test error-check code

--- parsere/tests/tests.rs
+++ parsere/tests/tests.rs
@@ -15,21 +15,29 @@ fn works() {
     assert_eq!(eg, Eg { x: 50, s: "xyzzy".to_string() });
 }
 
+macro_rules! error_check {
+    ($struc:ident, $txt:expr, $err:pat => $body:tt) => {
+        match $struc::parse_re($txt) {
+            Err(e) => {
+                match e.downcast_ref() {
+                    Some($err) => $body,
+                    Some(e) => panic!("wrong error: {:?}", e),
+                    None => panic!("downcast fail: {:?}", e),
+                }
+            }
+            Ok(eg) => panic!("no error: {:?}", eg),
+        }
+    };
+}
+
 #[test]
 fn match_err() {
     const TXT: &str = "50 xyzzy ";
-    match Eg::parse_re(TXT) {
-        Err(e) => {
-            match e.downcast_ref() {
-                Some(&parsere::Error::Mismatch { re, ref txt }) => {
-                    assert_eq!(re, RE);
-                    assert_eq!(txt, TXT);
-                }
-                Some(e) => panic!("wrong error: {:?}", e),
-                None => panic!("downcast fail: {:?}", e),
-            }
+    error_check! { Eg, TXT,
+        parsere::Error::Mismatch { re, txt } => {
+            assert_eq!(re, &RE);
+            assert_eq!(txt, &TXT);
         }
-        Ok(eg) => panic!("no error: {:?}", eg),
     }
 }
 
@@ -45,19 +53,12 @@ struct BadReEg {
 #[test]
 fn group_err() {
     const TXT: &str = "50";
-    match BadReEg::parse_re(TXT) {
-        Err(e) => {
-            match e.downcast_ref() {
-                Some(&parsere::Error::NoGroup { re, ref txt, index }) => {
-                    assert_eq!(re, BADRE);
-                    assert_eq!(txt, TXT);
-                    assert_eq!(index, 2);
-                }
-                Some(e) => panic!("wrong error: {:?}", e),
-                None => panic!("downcast fail: {:?}", e),
-            }
+    error_check! { BadReEg, TXT,
+        parsere::Error::NoGroup { re, txt, index } => {
+            assert_eq!(re, &BADRE);
+            assert_eq!(txt, &TXT);
+            assert_eq!(index, &2);
         }
-        Ok(eg) => panic!("no error: {:?}", eg),
     }
 }
 
@@ -70,16 +71,7 @@ struct NotReEg {
 #[test]
 fn not_re_err() {
     const TXT: &str = "50";
-    match NotReEg::parse_re(TXT) {
-        Err(e) => {
-            match e.downcast_ref() {
-                Some(&regex::Error::Syntax(_)) => {
-                    ()
-                }
-                Some(e) => panic!("wrong error: {:?}", e),
-                None => panic!("downcast fail: {:?}", e),
-            }
-        }
-        Ok(eg) => panic!("no error: {:?}", eg),
+    error_check! { NotReEg, TXT,
+        regex::Error::Syntax(_) => ()
     }
 }
